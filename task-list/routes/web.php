f4e0a50896cb8fbea88eab2ba311658d41c4e324
akhmed.gazimagomedov <akhmed.gazimagomedov@artif.com>
2026-06-29 12:58:09 +0200

    Add new Routes

--- task-list/routes/web.php
+++ task-list/routes/web.php
@@ -5,3 +5,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Add new Route
+Route::get('/hello', function () {
+    return 'Hello';
+}) -> name("hello");  // Add name to the Route
+
+// Route with parameter
+Route::get('/greet/{name}', function ($name) {
+    return 'Hello ' . $name . '!';
+});
+
+// Redirect Route
+Route::get('/hallo', function () {
+    return redirect() -> route('hello');    // Redirect by route name
+});
+
+// Return, when try to open not existing route or page
+Route::fallback(function () {
+    return 'Still got somewhere!';
+});
