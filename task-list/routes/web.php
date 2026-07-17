d10ec3515f214d9c7f37f026edbc25c71b99d529
akhmed.gazimagomedov <akhmed.gazimagomedov@artif.com>
2026-07-17 10:28:59 +0200

    Finish 'Deleting Data' section

--- task-list/routes/web.php
+++ task-list/routes/web.php
@@ -63,6 +63,13 @@ Route::put('/tasks/{task}', function (Task $task, TaskRequest $request) {
   
 })->name('tasks.update');
 
+Route::delete('/tasks/{task}', function (Task $task) {
+  $task->delete();
+  
+  return redirect()->route('tasks.index')
+    ->with('success', 'Task deleted successfully!');
+})->name('tasks.destroy');
+
 
 //// Add new Route
 //Route::get('/hello', function () {
