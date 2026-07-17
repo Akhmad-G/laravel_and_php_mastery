2996f8bdc87c1f720c57794549bf690ea9476fac
akhmed.gazimagomedov <akhmed.gazimagomedov@artif.com>
2026-07-17 11:14:07 +0200

    Finish 'Adding Pagination' section

--- task-list/resources/views/index.blade.php
+++ task-list/resources/views/index.blade.php
@@ -2,28 +2,6 @@
 
 @section('title', 'The list of tasks')
 
-
-{{--@isset($name)--}}
-{{--    <div>The name is: {{$name}}</div>--}}
-{{--@endisset--}}
-
-
-{{--<div>--}}
-{{--    @if(count($tasks))--}}
-{{--        <div>There are tasks!</div>--}}
-{{--    @else--}}
-{{--        <div>There are no tasks!</div>--}}
-{{--    @endif--}}
-{{--</div>--}}
-
-
-{{--<div>--}}
-{{--    @foreach($tasks as $task)--}}
-{{--        <p>{{ $task -> title }}</p>--}}
-{{--    @endforeach--}}
-{{--</div>--}}
-
-
 @section('content')
   @forelse($tasks as $task)
     <div>
@@ -32,4 +10,10 @@
   @empty
     <p>No tasks</p>
   @endforelse
+  
+  @if ($tasks->count())
+    <div>
+      {{ $tasks->links() }}
+    </div>
+  @endif
 @endsection
