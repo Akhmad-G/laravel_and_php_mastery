24dd7d2f23151f3914480387a0da87e753cfa6b7
akhmed.gazimagomedov <akhmed.gazimagomedov@artif.com>
2026-07-20 10:55:50 +0200

    Styling with Tailwind CSS in progress

--- task-list/resources/views/index.blade.php
+++ task-list/resources/views/index.blade.php
@@ -3,21 +3,22 @@
 @section('title', 'The list of tasks')
 
 @section('content')
-  <div>
-    <a href="{{ route('tasks.create') }}">Add Task!</a>
-  </div>
+  <nav class="mb-4">
+    <a href="{{ route('tasks.create') }}" class="link">Add Task!</a>
+  </nav>
   
   @forelse($tasks as $task)
     <div>
-      <a href="{{ route('tasks.show', ['task' => $task -> id ]) }}">{{ $task -> title }}</a>
+      <a href="{{ route('tasks.show', ['task' => $task -> id ]) }}"
+        @class(['line-through' => $task->completed])>{{ $task -> title }}</a>
     </div>
   @empty
     <p>No tasks</p>
   @endforelse
   
   @if ($tasks->count())
-    <div>
+    <nav class="mt-4">
       {{ $tasks->links() }}
-    </div>
+    </nav>
   @endif
 @endsection
